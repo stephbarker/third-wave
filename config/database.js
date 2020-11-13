17a6079389a0bdb1d7398437579d2c1c0d334e7f
Steph Barker <sbarkerdrums@gmail.com>
2020-11-13 15:11:07 -0800

    connects to MongoDB

--- config/database.js
+++ config/database.js
@@ -1,8 +1,20 @@
 // TODO
 
-
 // require the mongoose module
+const mongoose = require('mongoose');
+
 // set up a shortcut variable (db)
-// create a connectionURI variable and assign your MongoDB Atlas URI to it as a string
+const db = mongoose.connection;
+
 // connect to mongoDB
+mongoose.connect('mongodb+srv://admin:abceasyas123@cluster0.quwlv.mongodb.net/third-wave?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+});
+
 // set up a listener to alert us when we're connected
+db.on('connected', function() {
+    console.log(`Connected to MongoDB on ${db.host}:${db.port}`);
+});
+
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // Require our modules
 const express = require('express');
 const morgan = require('morgan');
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // set up the express app
 const app = express();
